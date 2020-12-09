50ba931f8eb2541603b1095620d051b7dc3b1ce4
Egor Bakanov <egorbakanov@gmail.com>
2020-12-09 22:25:41 +0700

    Add lines

---
+++ Assets/Scripts/Lines.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Lines : MonoBehaviour
+{
+    public Detector detector;
+    public Vector3 offset;
+    public LineRenderer red;
+    public LineRenderer blue;
+    [Range(0, 5)] public int blueAmount = 1;
+    [Range(0, 180f)] public float angle = 50f;
+    public LayerMask mask;
+
+    private Transform detectorTransform;
+
+    private void Start()
+    {
+        detectorTransform = detector.transform;
+        red.positionCount = 2;
+    }
+
+    private void Update()
+    {
+        var redStart = detectorTransform.localPosition + offset;
+        red.SetPosition(0, redStart);
+
+        var redDir = new Vector3(0, -Mathf.Sin(Mathf.Deg2Rad * angle), Mathf.Cos(Mathf.Deg2Rad * angle));
+        Physics.Raycast(redStart, redDir, out var hit, 20f, mask);
+        var blueStart = hit.point;
+        var blueDir = hit.normal;
+        red.SetPosition(1, blueStart);
+        blue.positionCount = blueAmount + 1;
+        blue.SetPosition(0,blueStart);
+
+        for (int i = 1; i <= blueAmount; i++)
+        {
+            Physics.Raycast(blueStart, blueDir, out hit, 20f, mask);
+            blueStart = hit.point;
+            blueDir = hit.normal;
+            blue.SetPosition(i, blueStart);
+        }
+    }
+}
